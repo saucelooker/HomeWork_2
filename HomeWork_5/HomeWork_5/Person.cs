faa899ccc867280222e0aa1c5abe3d4cdb0f49f2
Sergey <8may.sm@gmail.com>
2021-06-02 19:19:47 +0300

    Added 3 and 4 tasks.

---
+++ HomeWork_5/HomeWork_5/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace HomeWork_5
+{
+    public class Person
+    {
+
+        public Person()
+        {
+        }
+        public string _fullName { get; set; }
+        public string _position { get; set; }
+        public string _email { get; set; }
+        public string _phoneNumber { get; set; }
+        public int _salary { get; set; }
+        public int _age { get; set; }
+
+        public Person(string fullName,string position,string email,string phoneNumber,int salary,int age )
+        {
+            _fullName = fullName;
+            _position = position;
+            _email = email;
+            _phoneNumber = phoneNumber;
+            _salary = salary;
+            _age = age;
+        }
+        public void PrintPerson()
+        {
+            Console.Write($"{_fullName} ");
+            Console.Write($"{_position} ");
+            Console.Write($"{_email} ");
+            Console.Write($"{_phoneNumber} ");
+            Console.Write($"{_salary} ");
+            Console.WriteLine(_age);
+        }
+    }
+}
